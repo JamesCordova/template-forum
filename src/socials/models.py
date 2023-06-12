e174676cbca69d4719c5d2a4c7043f02a87ac08f
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-12 18:18:56 -0500

    Inheritance to Post for more efficient

--- src/socials/models.py
+++ src/socials/models.py
@@ -18,22 +18,23 @@ class Tag(models.Model):
 class Topic(Base):
     name = models.CharField(max_length=255)
 
-class Post(Base):
-    title = models.CharField(max_length=255)
+class PostBase(Base):
     content = MarkdownxField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    votes = models.IntegerField(default=0)
+    
+    class Meta:
+        ordering = ['votes', '-created_at']
+    
+class Post(PostBase):
+    title = models.CharField(max_length=255)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default=0)
     tags = models.ManyToManyField(Tag)
 
-class SubPost(Base):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+class SubPost(PostBase):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 class Response(Base):
     content = models.TextField()

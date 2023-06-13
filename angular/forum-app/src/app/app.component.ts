c9ddf7af01263d79ce6633dba511922c7a0985ef
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-12 21:00:00 -0500

    Changes to connect angular with django talking about angular

--- angular/forum-app/src/app/app.component.ts
+++ angular/forum-app/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'forum-app';
-}
+  posts = [{id:1,title:'Just example', content: 'Something', author: 'admin'},{id:2,title:'An example', content: "Really something",author: 'admin'}];
+  constructor(private api:ApiService) {
+    this.getPosts();
+  }
+  getPosts = () => {
+    this.api.getAllPosts().subscribe (
+      (data: any) => {
+        console.log(data);
+        this.posts = data; //data.results;
+      },
+      (error: any) => {
+        console.log(error);
+      } 
+      ) 
+    }
+} 
+      
